55dcf2e517dc8aeddd6c6263ee9781960a227625
Dibimo <diogobissolimoreno@gmail.com>
2021-03-30 13:15:56 -0300

    Test changes

--- mainQuestionario.js
+++ mainQuestionario.js
@@ -2,6 +2,7 @@ function main(){
     var form = document.querySelector("#formulario");
     console.log(form.resposta_hemorragia.value);
     console.log(form.resposta_tuberculose.value);
+    console.log(form.resposta_fumante.value);
 
     var radios = document.querySelectorAll('input[type="radio"]');
     radios.forEach(function (radio) {
@@ -9,4 +10,16 @@ function main(){
             console.log(radio.value);
         });
     });
+
+    var radioFumante = document.querySelectorAll('input[name="resposta_fumante"]');
+    radioFumante.forEach(function (radio) {
+        radio.addEventListener("click",function () {
+            var frequencia = document.querySelector("#frequencia");
+            if(radio.value == "true"){
+                frequencia.style.display = 'inline';
+                return;
+            }
+            frequencia.style.display = 'none';
+        })
+    })
 }
