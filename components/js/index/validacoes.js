0b706e567ad5db6b91e45708d088ad3ffe680b30
Dibimo <diogobissolimoreno@gmail.com>
2021-04-02 20:32:27 -0300

    Added function for validation paciente

--- components/js/index/validacoes.js
+++ components/js/index/validacoes.js
@@ -15,18 +15,30 @@ function validacaoCPF(cpf) {
     for (let i = 10; i > 1; i--) {
         soma += parseInt(cpf[10 - i]) * i;
     }
-    if (((soma * 10) % 11) == cpf[9]) {
+    if (((soma * 10) % 11) == cpf[9]) { //se o primeiro digito verificador bater
         soma = 0;
         for (let i = 11; i > 1; i--) {
             soma += parseInt(cpf[11 - i]) * i;
         }
-        if (((soma * 10) % 11) == cpf[10]) {
-            console.log('valido')
+        if (((soma * 10) % 11) == cpf[10]) {//se o segundo digito verificador bater
+            return true;
         } else {
-            console.log('não vailido');
+            return false;
         }
     } else {
-        console.log('num virou');
+        return false;
     }
+
+}
+
+function verificaPacienteValido(paciente) {
+    var invalidades = [];
+
+    var cpfValido = validacaoCPF(paciente.cpf);
+    if(!cpfValido){
+        invalidades.push("CPF");
+    }
+
+
 
 }
