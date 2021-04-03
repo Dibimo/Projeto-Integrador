0276613ad415b622d7d7d781fe6fbc1dc64392f7
Dibimo <diogobissolimoreno@gmail.com>
2021-04-03 19:54:03 -0300

    Added listener button

--- components/js/anamnese/main.js
+++ components/js/anamnese/main.js
@@ -1,8 +1,10 @@
 function main(){
     var form = document.querySelector("#formulario");
-    console.log(form.resposta_hemorragia.value);
-    console.log(form.resposta_tuberculose.value);
-    console.log(form.resposta_fumante.value);
+    var botao = document.querySelector("#enviar");
+    botao.addEventListener("click",function (event) {
+        event.preventDefault();
+        obtemAnamnese(form);
+    })
 
     var radios = document.querySelectorAll('input[type="radio"]');
     radios.forEach(function (radio) {
