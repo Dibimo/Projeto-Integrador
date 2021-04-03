745c33c8d05ed3fbbf0b3be10a0a8c5d158449c0
Dibimo <diogobissolimoreno@gmail.com>
2021-04-03 13:43:03 -0300

    Added code for tests

--- components/php/send_database.php
+++ components/php/send_database.php
@@ -1,5 +1,9 @@
 <?php
+    require_once 'Paciente.php';
+    $pacienteDados = (json_decode( $_POST['buffer'],true));
+    //var_dump($paciente['dadosPessoais']['nome']);
+    $paciente = new Paciente($pacienteDados);
 
-    $paciente = (json_decode( $_POST['buffer'],true));
-    var_dump($paciente['dadosPessoais']['nome']);
+    $paciente->teste();
+
 ?>
