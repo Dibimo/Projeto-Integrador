4d6053577cb484a8df2f20a1c1ce86aa77a448bf
Dibimo <diogobissolimoreno@gmail.com>
2021-04-03 11:04:53 -0300

    Class paciente added, but not finished

---
+++ components/php/Paciente.php
@@ -0,0 +1,27 @@
+<?php
+    class Paciente
+    {
+        //dados pessoais
+        private string $nome;
+        private string $data_nascimento;
+        private string $sexo;
+        private string $escolaridade;
+        private string $profissao;
+        private string $rg;
+        private string $estado_civil;
+        private string $naturalidade;
+        private string $nomeMae;
+        private string $nomePai;
+        private int $contato;
+        private int $cpf;
+
+        //dados fisicos
+        private float $peso;
+        private float $altura;
+        private string $cor;
+        //endereco completo
+    }
+
+
+
+?>
