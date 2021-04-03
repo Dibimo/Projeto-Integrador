f2e32f701a6614eadd8336be68c9c62e5d24cf15
Dibimo <diogobissolimoreno@gmail.com>
2021-04-03 13:40:57 -0300

    Construct method added

--- components/php/Paciente.php
+++ components/php/Paciente.php
@@ -22,6 +22,44 @@
         private string $cor;
         //endereco completo
         private Endereco $endereco;
+
+        public function __construct($dados)
+        {
+            //dados pessoais
+            $this->nome = $dados['dadosPessoais']['nome'];
+            $this->data_nascimento = $dados['dadosPessoais']['data_nascimento'];
+            $this->sexo = $dados['dadosPessoais']['sexo'];
+            $this->escolaridade = $dados['dadosPessoais']['escolaridade'];
+            $this->profissao = $dados['dadosPessoais']['profissao'];
+            $this->rg = $dados['dadosPessoais']['rg'];
+            $this->estado_civil = $dados['dadosPessoais']['estado_civil'];
+            $this->naturalidade = $dados['dadosPessoais']['naturalidade'];
+            $this->nomeMae = $dados['dadosPessoais']['mae'];
+            $this->nomePai = $dados['dadosPessoais']['pai'];
+            $this->cpf = $dados['dadosPessoais']['cpf'];
+
+            //dados fisicos
+            $this->peso = $dados['dadosFisicos']['peso'];
+            $this->altura = $dados['dadosFisicos']['altura'];
+            $this->cor = $dados['dadosFisicos']['cor'];
+
+            //endereço
+            $this->endereco = new Endereco(
+                $dados['enderecoCompleto']['endereco'],
+                $dados['enderecoCompleto']['complemento'],
+                $dados['enderecoCompleto']['cidade'],
+                $dados['enderecoCompleto']['bairro'],
+                $dados['enderecoCompleto']['estado_moradia'],
+                $dados['enderecoCompleto']['numero'],
+                $dados['enderecoCompleto']['cep']
+            );
+        }
+
+        public function teste(): void
+        {
+            echo ($this->endereco->toString());
+        }
+
     }
 
 
