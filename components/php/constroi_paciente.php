5e6d04208bcf589bcad5db60a0f7e5f9b693ce90
Dibimo <diogobissolimoreno@gmail.com>
2021-04-04 19:36:34 -0300

    Testes com o banco adicionados
    > Os testes são apenas para verificar se a conexão ocorre de forma adequada
    > Até mais avanços no projeto, essa parte do código ficará comentada

--- components/php/constroi_paciente.php
+++ components/php/constroi_paciente.php
@@ -1,9 +1,28 @@
 <?php
-    session_start();
     require_once 'Classes/Paciente.php';
+    session_start();
     $pacienteDados = (json_decode( $_POST['buffer'],true));
     //var_dump($paciente['dadosPessoais']['nome']);
     $paciente = new Paciente($pacienteDados);
+
+    /*
+    /*TESTES COM O BANCO DE DADOS*/
+    //-------------------------------
+    /*CONEXÃO COM O BANCO*/
+    // $servidor = "localhost";
+    // $usuario  = "root";
+    // $senha    = "@dibimo7";
+    // $banco    = "banco_pacientes";
+
+    // $conexao = new mysqli($servidor,$usuario,$senha,$banco);
+
+    // /*COMANDOS*/
+    // $comandoSQL = "INSERT INTO pacientes (nome, data_nascimento, sexo, cpf)
+    //     VALUES ('{$paciente->getNome()}','{$paciente->getData_nascimento()}','{$paciente->getSexo()}','{$paciente->getCpf()}');
+    // ";
+    // /*ENVIO DOS DADOS*/ 
+    // $conexao->query($comandoSQL); 
+    
     $_SESSION['paciente'] = $paciente;
     header("Location: anamnese.php");
 
