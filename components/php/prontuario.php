6055fc115a3e55ed19755cebc5e589e75c4d7e3f
Dibimo <diogobissolimoreno@gmail.com>
2021-04-05 14:36:01 -0300

    Alterações para testes

--- components/php/prontuario.php
+++ components/php/prontuario.php
@@ -2,10 +2,13 @@
 
 
     require_once 'Classes/Paciente.php';
+    require_once 'Classes/Anamnese.php';
 
     session_start();
+    $paciente = $_SESSION['paciente'];
+    echo ($paciente->getCpf()."\n");
     //var_dump( $_SESSION['paciente']);
-    var_dump($_POST['paciente']);
+    var_dump($_POST['buffer']);
 
     
 
