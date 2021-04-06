59bbb75a9d6d80ad66b2a475fe9ca40f12d03372
Dibimo <diogobissolimoreno@gmail.com>
2021-04-05 23:56:04 -0300

    aa

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,60 +8,61 @@
     <link rel="stylesheet" href="/components/css/index/style.css">
     <title>Document</title>
 </head>
+
 <body>
-    <div style="background-color: rgb(255, 253, 253);" >
-        
+    <div style="background-color: rgb(255, 253, 253);">
+
         <img src="/Assets/img_logo.png" height="200" width="300" style="padding-left: 30px;">
-        
+
         <h2><strong>FICHA DE IDENTIFICAÇÃO DO PACIENTE</strong></h2>
-        
+
         <div style="padding-right: 3%; padding-left: 3%;">
 
             <form id="formulario" action="components/php/constroi_paciente.php" method="post" class="borda">
-                
+
                 <div>
                     <label for="nome_completo">Nome:</label>
-                    <input type="text"  name="nome_completo" id="nome_completo" class="campoAlfabetico obrigatorio">
+                    <input type="text" name="nome_completo" id="nome_completo" class="campoAlfabetico obrigatorio">
                 </div>
-                
+
                 <div>
                     <label for="data_nascimento">DN:</label>
-                    <input type="date"  name="data_nascimento" id="data_nascimento" class="campo obrigatorio">
-                    
+                    <input type="date" name="data_nascimento" id="data_nascimento" class="campo obrigatorio">
+
                     <label for="sexo">Sexo:</label>
                     <select name="sexo" id="sexo" class="campo obrigatorio">
                         <option value="">selecione</option>
                         <option value="m">Masculino</option>
                         <option value="f">Feminino</option>
-                    </select>                
-                
-                    <label for="peso">Peso (Kg):</label>                
-                    <input type="text" name="peso" id="peso" class="campoNumerico obrigatorio"> 
-                    
+                    </select>
+
+                    <label for="peso">Peso (Kg):</label>
+                    <input type="text" name="peso" id="peso" class="campoNumerico obrigatorio">
+
 
                     <label for="altura">Altura</label>
                     <input type="text" name="altura" id="altura" class="campoNumerico obrigatorio">
 
                     <label for="cor">Cor:</label>
-                    <select name="cor" id="cor" class="campo obrigatorio">                    
+                    <select name="cor" id="cor" class="campo obrigatorio">
                         <option value="">selecione</option>
                         <option value="branco">Branco</option>
                         <option value="pardo">Pardo</option>
-                        <option value="negro">Negro</option>                    
+                        <option value="negro">Negro</option>
                     </select>
                 </div>
-                                
+
                 <div>
                     <label for="escolaridade">Escolaridade</label>
-                    <select name="escolaridade" id="cor" class="campo obrigatorio">                    
+                    <select name="escolaridade" id="cor" class="campo obrigatorio">
                         <option value="">selecione</option>
                         <option value="infantil">Educação Infantil</option>
                         <option value="fundamental">Ensino Fundamental</option>
-                        <option value="medio">Ensino Medio</option>                    
-                        <option value="superior">Enseino Superior</option>                    
-                        <option value="posGraduacao">Pós graduação</option>                    
-                        <option value="mestrado">Mestrado</option>                    
-                        <option value="doutorado">Doutorado</option>                    
+                        <option value="medio">Ensino Medio</option>
+                        <option value="superior">Enseino Superior</option>
+                        <option value="posGraduacao">Pós graduação</option>
+                        <option value="mestrado">Mestrado</option>
+                        <option value="doutorado">Doutorado</option>
                     </select>
 
                     <label for="profissao">Profissão</label>
@@ -119,12 +121,12 @@
                         <option value="sp">São Paulo</option>
                         <option value="se">Sergipe</option>
                         <option value="to">Tocantins</option>
-                    </select>                
+                    </select>
                 </div>
 
                 <div>
-                    <label for="filiacao">Filiação</label> <br>
-                    
+                    <label for="filiacao">Filiação</label> <br></br>
+
                     <label for="pai">Pai</label>
                     <input type="text" name="pai" id="pai" class="campoAlfabetico obrigatorio">
                     <label for="paiNaturalidade">Naturalidade</label>
@@ -155,59 +157,59 @@
                     <label for="complemento">Complemento</label>
                     <input type="texto" name="complemento" id="complemento" class="campo">
                 </div>
-                
+
                 <div>
                     <label for="bairro">Bairro</label>
                     <input type="texto" name="bairro" id="bairro" class="campoAlfabetico obrigatorio">
                 </div>
-                
+
                 <div>
                     <label for="cep">CEP</label>
                     <input type="texto" name="cep" id="cep" class="campoNumerico obrigatorio">
                 </div>
-                    <label for="cidade">Cidade</label>
-                    <input type="texto" name="cidade" id="cidade" class="campoAlfabetico obrigatorio">
-                    <div>
-                        <label for="estados">Estado:</label>
-                        <select name="estados_moradia" class="campo obrigatorio">
-                            <option value="">selecione--</option>
-                            <option value="ac">Acre</option>
-                            <option value="al">Alagoas</option>
-                            <option value="ap">Amapá</option>
-                            <option value="am">Amazonas</option>
-                            <option value="ba">Bahia</option>
-                            <option value="ce">Ceará</option>
-                            <option value="df">Distrito Federal</option>
-                            <option value="es">Espírito Santo</option>
-                            <option value="go">Goiás</option>
-                            <option value="ma">Maranhão</option>
-                            <option value="mt">Mato Grosso</option>
-                            <option value="ms">Mato Grosso do Sul</option>
-                            <option value="mg">Minas Gerais</option>
-                            <option value="pa">Pará</option>
-                            <option value="pb">Paraíba</option>
-                            <option value="pr">Paraná</option>
-                            <option value="pe">Pernambuco</option>
-                            <option value="pi">Piauí</option>
-                            <option value="rj">Rio de Janeiro</option>
-                            <option value="rn">Rio Grande do Norte</option>
-                            <option value="rs">Rio Grande do Sul</option>
-                            <option value="ro">Rondônia</option>
-                            <option value="rr">Roraima</option>
-                            <option value="sc">Santa Catarina</option>
-                            <option value="sp">São Paulo</option>
-                            <option value="se">Sergipe</option>
-                            <option value="to">Tocantins</option>
-                        </select>
-                    </div>
+                <label for="cidade">Cidade</label>
+                <input type="texto" name="cidade" id="cidade" class="campoAlfabetico obrigatorio">
+                <div>
+                    <label for="estados">Estado:</label>
+                    <select name="estados_moradia" class="campo obrigatorio">
+                        <option value="">selecione--</option>
+                        <option value="ac">Acre</option>
+                        <option value="al">Alagoas</option>
+                        <option value="ap">Amapá</option>
+                        <option value="am">Amazonas</option>
+                        <option value="ba">Bahia</option>
+                        <option value="ce">Ceará</option>
+                        <option value="df">Distrito Federal</option>
+                        <option value="es">Espírito Santo</option>
+                        <option value="go">Goiás</option>
+                        <option value="ma">Maranhão</option>
+                        <option value="mt">Mato Grosso</option>
+                        <option value="ms">Mato Grosso do Sul</option>
+                        <option value="mg">Minas Gerais</option>
+                        <option value="pa">Pará</option>
+                        <option value="pb">Paraíba</option>
+                        <option value="pr">Paraná</option>
+                        <option value="pe">Pernambuco</option>
+                        <option value="pi">Piauí</option>
+                        <option value="rj">Rio de Janeiro</option>
+                        <option value="rn">Rio Grande do Norte</option>
+                        <option value="rs">Rio Grande do Sul</option>
+                        <option value="ro">Rondônia</option>
+                        <option value="rr">Roraima</option>
+                        <option value="sc">Santa Catarina</option>
+                        <option value="sp">São Paulo</option>
+                        <option value="se">Sergipe</option>
+                        <option value="to">Tocantins</option>
+                    </select>
+                </div>
 
-                    <h2><strong>(obrigatório o preenchimento ser realizado pela recepção)</strong></h2>
+                <h2><strong>(obrigatório o preenchimento ser realizado pela recepção)</strong></h2>
 
                 <div>
                     <input type="hidden" id="buffer" name="buffer">
                 </div>
 
-                <br>
+                <br></br>
 
                 <div style="padding-left: 3%;">
                     <button id="botao-cadastrar" class="botao">Cadastrar</button>
@@ -225,6 +227,7 @@
             main();
         </script>
     </div>
-    
+
 </body>
+
 </html>
