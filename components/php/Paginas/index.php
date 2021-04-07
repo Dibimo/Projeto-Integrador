1402f66b8cb465d642055f3c2698f4b56177c5f7
Dibimo <diogobissolimoreno@gmail.com>
2021-04-07 15:53:24 -0300

    Testes de consulta no banco de dados

--- components/php/Paginas/index.php
+++ components/php/Paginas/index.php
@@ -11,16 +11,22 @@
      $conexao = new mysqli($servidor,$usuario,$senha,$banco);
 
      /*COMANDOS*/
-     $comandoSQL = "SELECT * FROM  teste";
+     $comandoSQL = "SELECT * FROM  teste where nome='diogo'";
      /*ENVIO DOS DADOS*/ 
     // var_dump($conexao->query($comandoSQL)->num_rows); 
     $resultado = mysqli_query($conexao,$comandoSQL);
     // var_dump($resultado);
     //$linhas = mysqli_fetch_assoc($resultado);
     //var_dump($linhas);
+    $linha = mysqli_fetch_assoc($resultado);
+    if($linha){
+        echo 'existe sim, tá safe';
+    }else{
+        echo 'mano...não tem nada aqui não...será que não é na camada de cima?';
+    }
     while ($linha = mysqli_fetch_assoc($resultado)) {
         var_dump($linha['nome']);
     }
     
-
+   
 ?>
