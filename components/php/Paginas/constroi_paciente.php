5ee9ae840cabd0b96a090d60d1cdbed52a1278ac
Dibimo <diogobissolimoreno@gmail.com>
2021-04-08 10:14:09 -0300

    Página reconstruida para o padrão

--- components/php/Paginas/constroi_paciente.php
+++ components/php/Paginas/constroi_paciente.php
@@ -5,33 +5,10 @@
     $pacienteDados = (json_decode( $_POST['buffer'],true));
     //var_dump($paciente['dadosPessoais']['nome']);
     $paciente = new Paciente($pacienteDados);
-
-    $jaTem = verificaPacienteExistente('36700137845');
-
-    var_dump($jaTem);
-    
-    //inserePaciente($paciente);
-    /*
-    /*TESTES COM O BANCO DE DADOS*/
-    //-------------------------------
-    /*CONEXÃO COM O BANCO*/
-    // $servidor = "localhost";
-    // $usuario  = "root";
-    // $senha    = "@dibimo7";
-    // $banco    = "banco_pacientes";
-
-    // $conexao = new mysqli($servidor,$usuario,$senha,$banco);
-
-    // /*COMANDOS*/
-    // $comandoSQL = "INSERT INTO pacientes (nome, data_nascimento, sexo, cpf)
-    //     VALUES ('{$paciente->getNome()}','{$paciente->getData_nascimento()}','{$paciente->getSexo()}','{$paciente->getCpf()}');
-    // ";
-    // /*ENVIO DOS DADOS*/ 
-    // $conexao->query($comandoSQL); 
     
 
-    //$_SESSION['paciente'] = $paciente;
-    //header("Location: anamnese.php");
+    $_SESSION['paciente'] = $paciente; //salvando paciente na sessão
+    header("Location: anamnese.php");
 
     die();
 ?>
