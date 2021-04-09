c495a61f6df960d291798f54964d1d2460f5d7ea
Dibimo <diogobissolimoreno@gmail.com>
2021-04-09 19:07:19 -0300

    Campos adicionados

--- components/php/Paginas/prontuario.php
+++ components/php/Paginas/prontuario.php
@@ -1,21 +1,21 @@
 <?php
 
-    require_once dirname(__DIR__,1). '\\Funcao\\manipular_banco.php';
-    if(verificaPacienteExistente('36700137845')){
-        // echo 'existe';
-        // echo '<script> var dadosPessoais = ""+'.obtemDadosTabela('36700137845','pacientes')."</script>";
-        $dadosPessoais = obtemDadosTabela('36700137845','pacientes');
-        var_dump(json_encode(obtemDadosTabela('36700137845','enderecos')));
-        // var_dump(obtemDadosTabela('36700137845','anamnese_geral'));
-        // var_dump(obtemDadosTabela('36700137845','anamnese_infantil'));
-    }else{
-        // echo 'não existe';
-    }
-    // $prontuario = obtemProntuario('10054547016');
+require_once dirname(__DIR__, 1) . '\\Funcao\\manipular_banco.php';
+if (verificaPacienteExistente('36700137845')) {
+    // echo 'existe';
+    // echo '<script> var dadosPessoais = ""+'.obtemDadosTabela('36700137845','pacientes')."</script>";
+    // var_dump(json_encode(obtemDadosTabela('36700137845','enderecos')));
+    // var_dump(obtemDadosTabela('36700137845','anamnese_geral'));
+    // var_dump(obtemDadosTabela('36700137845','anamnese_infantil'));
+} else {
+    // echo 'não existe';
+}
+// $prontuario = obtemProntuario('10054547016');
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -23,8 +23,30 @@
     <link rel="stylesheet" href="/components/css/prontuario/style.css">
     <title>Document</title>
 </head>
+
 <body>
-    
+    <h1>Dados do paciente</h1>
+    <div id="dadosPessoais">
+        <div><label class="campoPessoal" name="nome_paciente">Nome:</label> </div>
+        <div><label class="campoPessoal" name="data_nascimento">Data de Nascimento:</label> </div>
+        <div><label class="campoPessoal" name="sexo">Sexo: </label></div>
+        <div><label class="campoPessoal" name="escolaridade">Escolaridade: </label></div>
+        <div><label class="campoPessoal" name="profissao">Profissao: </label></div>
+        <div><label class="campoPessoal" name="rg_paciente">RG: </label></div>
+        <div><label class="campoPessoal" name="cpf_paciente">CPF: </label></div>
+        <div><label class="campoPessoal" name="estado_civil">Estado Civil: </label></div>
+        <div><label class="campoPessoal" name="natu">Naturalidade: </label></div>
+        <div><label class="campoPessoal" name="estado_nasc">Estado de Nascença: </label></div>
+        <div><label class="campoPessoal" name="tel_contato">Telefone de contato: </label></div>
+        <div><label class="campoPessoal" name="nome_pai">Nome do pai: </label></div>
+        <div><label class="campoPessoal" name="nome_mae">Nome da mãe: </label></div>
+        <div><label class="campoPessoal" name="natu_pai">Naturalidade do Pai: </label></div>
+        <div><label class="campoPessoal" name="natu_mae">Naturalidade da Mãe: </label></div>
+        <div><label class="campoPessoal" name="peso">Peso: </label></div>
+        <div><label class="campoPessoal" name="altura">Altura: </label></div>
+        <div><label class="campoPessoal" name="cor">Cor: </label></div>
+    </div>
+    <hr>
     <button id="adicionar">+</button>
     <table id="prontuarioTabela">
         <thead>
@@ -35,28 +57,21 @@
             </tr>
 
         </thead>
-        
+
         <tbody>
-            
+
         </tbody>
     </table>
 
     <script src="/components/js/prontuario/main.js"></script>
-    <script> 
+    <script>
         main();
-        // var prontuario ="" + <?php //echo $prontuario; ?>;
-        var manipulavel = <?=(obtemDadosTabela('36700137845', 'enderecos'));?>;
-        // var dadosTratados = prontuario.replace(/[":]/g,"");
-        // dadosTratados = dadosTratados.replace(/[,]/g," ");
-        // dadosTratados = dadosTratados.replace(/10054547016 /g,"");
-        // dadosTratados = dadosTratados.replace(/data_procedimento/g,"");
-        // dadosTratados = dadosTratados.replace(/cpf_paciente/g,"");
-        // dadosTratados = dadosTratados.replace(/procedimento/g,"");
-        // dadosTratados = dadosTratados.replace(/} {/g,"--");
-        // dadosTratados = dadosTratados.replace(/[{}]/g,"");
-        // dadosTratados = dadosTratados.split('--');
-        // var manipulavel = JSON.parse(prontuario);
+        var manipulavel = <?= (obtemDadosTabela('36700137845', 'pacientes')); ?>;
+
         console.log(manipulavel);
+        // console.log(Object.keys(manipulavel))
+        insereDadosPessoais(manipulavel);
     </script>
-</body> 
+</body>
+
 </html>
