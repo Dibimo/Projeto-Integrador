17cb20eae52c80dee0a13356e91df3f822d0df05
Dibimo <diogobissolimoreno@gmail.com>
2021-04-09 13:04:44 -0300

    Envio completo

--- components/php/Paginas/send_to_database.php
+++ components/php/Paginas/send_to_database.php
@@ -1,14 +1,18 @@
 <?php
 require_once dirname(__DIR__, 1) . '\\Classes\\Paciente.php';
 require_once dirname(__DIR__, 1) . '\\Classes\\Anamnese.php';
+require_once dirname(__DIR__, 1) . '\\Classes\\Endereco.php';
 require_once dirname(__DIR__, 1) . '\\Funcao\\manipular_banco.php';
 
 session_start();
 $paciente = $_SESSION['paciente']; //recebe paciente da página anterior anterior
 $anamneseDados = (json_decode($_POST['buffer'], true));
-//inserePaciente($paciente);
 $paciente->setAnamnese($anamneseDados); //salvando respostas no objeto
 
+inserePaciente($paciente);
+echo $paciente->getAnamnese()->getHistoriaGestacao();
+header("Location: prontuario.php"); //TROCAR
+die();
 
 
 
