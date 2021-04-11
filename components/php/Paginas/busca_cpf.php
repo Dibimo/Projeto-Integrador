cba0584f4ec91e0690f3c9c9e37c17f993a1a897
Dibimo <diogobissolimoreno@gmail.com>
2021-04-10 22:43:43 -0300

    Busca de CPF implementada

--- components/php/Paginas/busca_cpf.php
+++ components/php/Paginas/busca_cpf.php
@@ -1,17 +1,13 @@
 <?php
 require_once dirname(__DIR__, 1) . '\\Funcao\\manipular_banco.php';
 
-$cpf = filter_input(INPUT_POST, 'cpfInput', FILTER_SANITIZE_STRING);
-// $cpf = $_POST['pesquisaCpf'];
+$cpf = filter_input(INPUT_POST, 'cpf', FILTER_SANITIZE_STRING);
 
-var_dump($cpf);
-
-
-// $existe = verificaPacienteExistente($cpf);
-// if($existe){
-//     echo 'Usuário encontrado!';
-//     die();
-// }
-// echo 'Usuário não econtrado!'
+$existe = verificaPacienteExistente($cpf);
+if($existe){
+    echo 'Usuário encontrado!';
+    die();
+}
+echo 'Usuário não econtrado!'
 
 ?>
