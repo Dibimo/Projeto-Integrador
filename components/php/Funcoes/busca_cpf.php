e54a6163fecf202b0f0fd06632c6151df8da26d9
Dibimo <diogobissolimoreno@gmail.com>
2021-04-11 13:26:11 -0300

    Adicionada seesion_start()
    > Para manter o CPF entre as páginas

--- components/php/Funcoes/busca_cpf.php
+++ components/php/Funcoes/busca_cpf.php
@@ -2,7 +2,8 @@
 require_once dirname(__DIR__, 1) . '\\Funcoes\\manipular_banco.php';
 
 $cpf = filter_input(INPUT_POST, 'cpf', FILTER_SANITIZE_STRING);
-
+session_start();
+$_SESSION['cpf'] = $cpf;
 $existe = verificaPacienteExistente($cpf);
 if($existe){
     echo 'Usuário encontrado!';
