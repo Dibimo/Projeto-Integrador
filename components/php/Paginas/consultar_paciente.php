4bd97bb1713ed3162e701746ca8d8bc9853c764d
Dibimo <diogobissolimoreno@gmail.com>
2021-04-11 13:27:50 -0300

    Adicionado botão para redirecionamento para index.php

--- components/php/Paginas/consultar_paciente.php
+++ components/php/Paginas/consultar_paciente.php
@@ -3,16 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/components/css/consultar_paciente/animacao_buscando.css">
     <title>Consultar paciente</title>
 </head>
 <body>
     <h1>Informe o CPF do paciente</h1>
     
     <label>Pesquisar</label>
-    <input type="text" name="pesquisa" id="pesquisa" placeholder="00000000000">
-    <label id="resposta"><div class="lds-facebook"><div></div><div></div><div></div></div></label>
+    <input type="text" name="pesquisa" id="pesquisa" placeholder="00000000000" maxlength="11">
+    <label id="resposta"></label>
+     <label id="resposta"></label>
 
+    <hr>
+    <h1>Cadastrar paciente</h1>
+    <button id="irParaCadastro">Cadastrar</button>
     
     <script src="/components/js/consultar_paciente/main.js"></script>
     <script>main();</script>
