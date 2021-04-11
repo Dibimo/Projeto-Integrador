d83db80c986d142b28e6f90861f5a3f765e6d882
Dibimo <diogobissolimoreno@gmail.com>
2021-04-10 22:47:04 -0300

    Código melhor organizado

--- components/php/Paginas/consultar_paciente.php
+++ components/php/Paginas/consultar_paciente.php
@@ -1,5 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Consultar paciente</title>
+</head>
+<body>
+    <h1>Informe o CPF do paciênte</h1>
     
+    <label>Pesquisar</label>
+    <input type="text" name="pesquisa" id="pesquisa" placeholder="00000000000">
     
-   
-?>
+    <script src="/components/js/consultar_paciente/main.js"></script>
+    <script>main();</script>
+</body>
+</html>
