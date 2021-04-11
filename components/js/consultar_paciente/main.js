acd0f859bcdc944401385b098505231c20099683
Dibimo <diogobissolimoreno@gmail.com>
2021-04-10 23:39:19 -0300

    Listener ajustado para exibir a resposta

--- components/js/consultar_paciente/main.js
+++ components/js/consultar_paciente/main.js
@@ -1,21 +1,24 @@
 function main() {
-    var pesquisa = document.querySelector("#pesquisa");
     pesquisa.addEventListener("input",function () {
+        var pesquisa = document.querySelector("#pesquisa");
+        var resposta = document.querySelector("#resposta");
         var cpfInput = pesquisa.value;
-        
-        if (cpfInput!= '') {
+
+        if (cpfInput.length == 11) {
             var xml = new XMLHttpRequest();
             var dados = new FormData();
             dados.append('cpf',cpfInput);
             xml.onreadystatechange = function () {
                 
                 if(xml.readyState == 4 && xml.status==200){
-                    console.log(xml.responseText);
+                    resposta.textContent = (xml.responseText);
                 }
             };
 
             xml.open('POST','../../php/Paginas/busca_cpf.php',true);
             xml.send(dados);
+        }else{
+            resposta.textContent = '';
         }
     });
 }
