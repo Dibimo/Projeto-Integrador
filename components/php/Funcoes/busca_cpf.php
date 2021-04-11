8ee5327d0bbe126d447bcbc3d0323cc88c4eff0f
Dibimo <diogobissolimoreno@gmail.com>
2021-04-11 14:08:17 -0300

    Feito pequeno ajuste

--- components/php/Funcoes/busca_cpf.php
+++ components/php/Funcoes/busca_cpf.php
@@ -3,9 +3,9 @@ require_once dirname(__DIR__, 1) . '\\Funcoes\\manipular_banco.php';
 
 $cpf = filter_input(INPUT_POST, 'cpf', FILTER_SANITIZE_STRING);
 session_start();
-$_SESSION['cpf'] = $cpf;
 $existe = verificaPacienteExistente($cpf);
 if($existe){
+    $_SESSION['cpf'] = $cpf;
     echo 'Usuário encontrado!';
     die();
 }
