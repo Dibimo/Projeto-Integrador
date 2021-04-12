406ba694d77d74ac230dbb10b5c77f41e0d0ae17
Dibimo <diogobissolimoreno@gmail.com>
2021-04-11 21:26:17 -0300

    Tela de login implementada

---
+++ components/js/login/main.js
@@ -0,0 +1,6 @@
+function main() {
+    var botaoEntrar = document.querySelector("#botaoEntrar");
+    botaoEntrar.addEventListener('click',function () {
+        window.location.href = '/components/php/Paginas/consultar_paciente.php';
+    });
+}
