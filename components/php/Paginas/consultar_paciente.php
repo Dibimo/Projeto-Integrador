41034a7008aafa34b50c383161d217ab937689ff
Leonardo Bratfisch <sevalhosleonardo@gmail.com>
2021-04-12 22:23:11 -0300

    \Paginas\consultar_paciente: formatação feita

--- components/php/Paginas/consultar_paciente.php
+++ components/php/Paginas/consultar_paciente.php
@@ -3,21 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/components/css/public/style.css">
     <title>Consultar paciente</title>
 </head>
 <body>
-    <h1>Informe o CPF do paciente</h1>
     
-    <label>Pesquisar</label>
-    <input type="text" name="pesquisa" id="pesquisa" placeholder="00000000000" maxlength="11">
-    <label id="resposta"></label>
-     <label id="resposta"></label>
+    <div class="borda">
+        
+        <img src="/Assets/img_logo.png" height="200" width="300" style="padding-left: 30px;">
+    
+        <h1>Informe o CPF do paciente</h1>
+        
+        <label>Pesquisar</label>
+        <br>
+        <input type="text" name="pesquisa" id="pesquisa" placeholder="00000000000" maxlength="11" style="height: 5%; width: 100%; font-size: 20px;">
+        <label id="resposta"></label>
+        <label id="resposta"></label>
 
-    <hr>
-    <h1>Cadastrar paciente</h1>
-    <button id="irParaCadastro">Cadastrar</button>
-    
-    <script src="/components/js/consultar_paciente/main.js"></script>
-    <script>main();</script>
+        <hr>
+        <h1>Cadastrar paciente</h1>
+        <button id="irParaCadastro">Cadastrar</button>
+        
+        <script src="/components/js/consultar_paciente/main.js"></script>
+        <script>main();</script>
+    </div>
 </body>
 </html>
