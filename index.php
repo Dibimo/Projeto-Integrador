fd2a5da8e27b983114bb970ff772e5f52345849f
Leonardo Bratfisch <sevalhosleonardo@gmail.com>
2021-04-12 21:49:14 -0300

    \index.php: inicio da formatação feita no index.php

--- index.php
+++ index.php
@@ -3,19 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/components/css/index/style.css">
     <title>Login</title>
 </head>
 <body>
-    <h1>Login</h1>
-    <div>
-        <label for="login">Login</label>
-        <input type="text" name="login">
-        <label for="senha">Senha</label>
-        <input type="password" name="senha">
-        <button id="botaoEntrar">Entrar</button>
+    <div class="borda">
+        <h1>Login</h1>
+        <div>
+            <label for="login">Login</label>
+            <input type="text" name="login">
+            <label for="senha">Senha</label>
+            <input type="password" name="senha">
+            <button id="botaoEntrar">Entrar</button>
+        </div>
+        <script src="/components/js/index/main.js"></script>
+        <script>main();</script>
     </div>
-    <script src="/components/js/index/main.js"></script>
-    <script>main();</script>
 
 </body>
 </html>
