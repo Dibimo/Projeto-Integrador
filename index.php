d2656437b31413bca6691e201659246d0958934d
Leonardo Bratfisch <sevalhosleonardo@gmail.com>
2021-04-12 21:55:36 -0300

    \index.php: formatação base do index feita

--- index.php
+++ index.php
@@ -8,12 +8,16 @@
 </head>
 <body>
     <div class="borda">
+        <img src="/Assets/img_logo.png" height="200" width="300" style="padding-left: 30px;">
+        
         <h1>Login</h1>
         <div>
             <label for="login">Login</label>
-            <input type="text" name="login">
+            <input type="text" name="login" style="height: 5%; width: 92.9%; font-size: 20px;">
+            <br>
             <label for="senha">Senha</label>
-            <input type="password" name="senha">
+            <input type="password" name="senha" style="height: 5%; width: 92%; font-size: 20px;">
+            <br>
             <button id="botaoEntrar">Entrar</button>
         </div>
         <script src="/components/js/index/main.js"></script>
