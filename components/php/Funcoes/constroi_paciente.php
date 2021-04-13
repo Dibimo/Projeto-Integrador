ef6365c1de928c54229ed053ade25f85efa25f13
Dibimo <diogobissolimoreno@gmail.com>
2021-04-13 12:09:43 -0300

    Função die() removida

--- components/php/Funcoes/constroi_paciente.php
+++ components/php/Funcoes/constroi_paciente.php
@@ -2,11 +2,9 @@
     require_once dirname(__DIR__,1).'\\Classes\\Paciente.php';
     session_start();
     $pacienteDados = (json_decode( $_POST['buffer'],true));
-    //var_dump($paciente['dadosPessoais']['nome']);
     $paciente = new Paciente($pacienteDados);
     
     $_SESSION['paciente'] = $paciente; //salvando paciente na sessão
     header("Location: ..\\Paginas\\anamnese.php");
 
-    die();
 ?>
