4604f7662b086b6705363b76b58ce40e2c5ef181
Dibimo <diogobissolimoreno@gmail.com>
2021-04-17 11:17:11 -0300

    Declaração de função defineEscopoCampos() melhorada

--- components/js/cadastrar_paciente/listeners.js
+++ components/js/cadastrar_paciente/listeners.js
@@ -6,11 +6,11 @@ function adicionaListenerCampos(campos) {
 
     }
 }
-function defineEscopoCampos(campos, expressao) {
+function defineEscopoCampos(campos, escopo) {
     for (let i = 0; i < campos.length; i++) {
         campos[i].addEventListener("input", function () {
             var texto = campos[i].value;
-            texto = texto.replace(expressao,"");
+            texto = texto.replace(escopo,"");
             campos[i].value = texto;
         });
 
