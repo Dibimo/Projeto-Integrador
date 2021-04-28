51055131348b90735f5bc9afae72dbd171e8f3eb
Dibimo <diogobissolimoreno@gmail.com>
2021-04-28 16:06:53 -0300

    Função para registrar paciente no banco adicionada

--- components/php/Funcoes/registrar_paciente.php
+++ components/php/Funcoes/registrar_paciente.php
@@ -1,7 +1,13 @@
 <?php
 require_once dirname(__DIR__, 1) . '\\Funcoes\\manipular_banco.php';
+require_once dirname(__DIR__, 1) . '\\Classes\\Paciente.php';
 
 
 $str_json = file_get_contents('php://input');
 
-var_dump(json_decode($str_json));
+$dados = json_decode($str_json, true);
+
+$paciente = new Paciente($dados);
+
+inserePaciente($paciente);
+
