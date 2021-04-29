a55f0729a39d1fc92a28c38a45cc45d9872a3460
Dibimo <diogobissolimoreno@gmail.com>
2021-04-29 12:21:12 -0300

    Função para registrar anamense implementada

---
+++ components/php/Funcoes/registrar_anamnese.php
@@ -0,0 +1,12 @@
+<?php
+require_once dirname(__DIR__, 1) . '\\Funcoes\\manipular_banco.php';
+require_once dirname(__DIR__, 1) . '\\Classes\\Paciente.php';
+
+
+$str_json = file_get_contents('php://input');
+
+$dados = json_decode($str_json, true);
+
+$paciente = new Paciente($dados);
+
+insereAnamnese($paciente);
