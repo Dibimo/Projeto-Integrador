3014f716458745fa1a2e4653c45bba0b98a0885f
Dibimo <diogobissolimoreno@gmail.com>
2021-05-11 12:35:32 -0300

    Adicionada funcionalidade para UPDATE no banco

--- components/php/Funcoes/manipular_banco.php
+++ components/php/Funcoes/manipular_banco.php
@@ -49,102 +49,194 @@ function insereAnamnese($paciente)
 {
     $conexao = novaConexao();
     $comandosSQL = Array();
-    
-    if ($paciente->getAnamnese()->getTipoParto()!='') {
-        $comandosSQL[] = "INSERT INTO anamnese_infantil
-        VALUES(
-            '{$paciente->getNome()}',
-            '{$paciente->getCpf()}',
-            '{$paciente->getAnamnese()->getHistoriaGestacao()}',
-            '{$paciente->getAnamnese()->getTipoParto()}',
-            '{$paciente->getAnamnese()->getProblemaParto()}',
-            '{$paciente->getAnamnese()->getAmamentacao()}',
-            '{$paciente->getAnamnese()->getAnestesia()}',
-            '{$paciente->getAnamnese()->getDoencaGrave()}',
-            '{$paciente->getAnamnese()->getVacinada()}',
-            '{$paciente->getAnamnese()->getSentou()}',
-            '{$paciente->getAnamnese()->getEngatinhou()}',
-            '{$paciente->getAnamnese()->getAndou()}',
-            '{$paciente->getAnamnese()->getFalou()}',
-            '{$paciente->getAnamnese()->getDificuldade()}',
-            '{$paciente->getAnamnese()->getAlegre()}',
-            '{$paciente->getAnamnese()->getTriste()}',
-            '{$paciente->getAnamnese()->getTimido()}',
-            '{$paciente->getAnamnese()->getTranquilo()}',
-            '{$paciente->getAnamnese()->getInquieto()}',
-            '{$paciente->getAnamnese()->getAssustado()}',
-            '{$paciente->getAnamnese()->getSono()}',
-            '{$paciente->getAnamnese()->getPosturaNormal()}',
-            '{$paciente->getAnamnese()->getPosturaAlterada()}',
-            '{$paciente->getAnamnese()->getFonacaoNormal()}',
-            '{$paciente->getAnamnese()->getDisturbiosFala()}',
-            '{$paciente->getAnamnese()->getParalisia()}',
-            '{$paciente->getAnamnese()->getEnurese()}',
-            '{$paciente->getAnamnese()->getEsfincteres()}',
-            '{$paciente->getAnamnese()->getAlimentacao()}',
-            '{$paciente->getAnamnese()->getSociabilidade()}',
-            '{$paciente->getAnamnese()->getTiques()}',
-            '{$paciente->getAnamnese()->getFobias()}',
-            '{$paciente->getAnamnese()->getAnsiedade()}',
-            '{$paciente->getAnamnese()->getMedo()}',
-            '{$paciente->getAnamnese()->getBirra()}',
-            '{$paciente->getAnamnese()->getCiumes()}',
-            '{$paciente->getAnamnese()->getObservacoes()}'
+    if(verificaAnamneseExistente($paciente->getCpf(), 'anamnese_geral')){
+        $comandosSQL[] = "UPDATE anamnese_geral
+        SET
+            queix_princ = '{$paciente->getAnamnese()->getQueixaPrincipal()}',
+            historico = '{$paciente->getAnamnese()->getHistoricoDoenca()}',
+            hemorragia = '{$paciente->getAnamnese()->getHemorragia()}',
+            reumatismo = '{$paciente->getAnamnese()->getReumatismo()}',
+            alergia = '{$paciente->getAnamnese()->getAlergia()}',
+            cardio = '{$paciente->getAnamnese()->getCardiovascular()}',
+            gastriste = '{$paciente->getAnamnese()->getGastrite()}',
+            diabetico = '{$paciente->getAnamnese()->getDiabetico()}',
+            desmaio = '{$paciente->getAnamnese()->getDesmaio()}',
+            tratamento = '{$paciente->getAnamnese()->getTratamento()}',
+            medicamento = '{$paciente->getAnamnese()->getMedicamento()}',
+            opercao = '{$paciente->getAnamnese()->getOperacao()}',
+            manias = '{$paciente->getAnamnese()->getManias()}',
+            depressao = '{$paciente->getAnamnese()->getDepressao()}',
+            tuberculose = '{$paciente->getAnamnese()->getTuberculose()}',
+            sarampo = '{$paciente->getAnamnese()->getSarampo()}',
+            sifilis = '{$paciente->getAnamnese()->getSifilis()}',
+            caxumba = '{$paciente->getAnamnese()->getCaxumba()}',
+            hepatite = '{$paciente->getAnamnese()->getHepatite()}',
+            varicela = '{$paciente->getAnamnese()->getVaricela()}',
+            aids = '{$paciente->getAnamnese()->getAids()}',
+            outra = '{$paciente->getAnamnese()->getOutra()}',
+            fumante = '{$paciente->getAnamnese()->getFumante()}',
+            fuman_freque = '{$paciente->getAnamnese()->getFrequencia()}',
+            labios = '{$paciente->getAnamnese()->getLabios()}',
+            mucosaj = '{$paciente->getAnamnese()->getMucosaJugal()}',
+            lingua = '{$paciente->getAnamnese()->getLingua()}',
+            soalho = '{$paciente->getAnamnese()->getSoalho()}',
+            palatod = '{$paciente->getAnamnese()->getPalatoDuro()}',
+            garganta = '{$paciente->getAnamnese()->getGarganta()}',
+            palatom = '{$paciente->getAnamnese()->getPalatoMole()}',
+            mucosaa = '{$paciente->getAnamnese()->getMucosaAlveolar()}',
+            gengiva = '{$paciente->getAnamnese()->getGengivas()}',
+            glandulas = '{$paciente->getAnamnese()->getGlandulasSalivares()}',
+            linfonodo = '{$paciente->getAnamnese()->getLinfonodos()}',
+            atm = '{$paciente->getAnamnese()->getAtm()}',
+            muscmas = '{$paciente->getAnamnese()->getMuscMastigadores()}',
+            oclusao = '{$paciente->getAnamnese()->getOclusao()}',
+            alteracoes = '{$paciente->getAnamnese()->getAlteracoesEncontradas()}',
+            diagnosp = '{$paciente->getAnamnese()->getDiagnosticoPresuntivo()}',
+            examec = '{$paciente->getAnamnese()->getExamesComplementares()}',
+            diagnosd = '{$paciente->getAnamnese()->getDiagnosticoDefinitivo()}',
+            planotrat = '{$paciente->getAnamnese()->getPlanoTratamento()}'
+        
+        WHERE cpf_paciente = '{$paciente->getCpf()}';";
+        if(verificaAnamneseExistente($paciente->getCpf(), 'anamnese_infantil')){
+            $comandosSQL[] = "UPDATE anamnese_geral
+            SET
+            historiag = '{$paciente->getAnamnese()->getHistoriaGestacao()}',
+            tipoparto = '{$paciente->getAnamnese()->getTipoParto()}',
+            problemap = '{$paciente->getAnamnese()->getProblemaParto()}',
+            amamentacao = '{$paciente->getAnamnese()->getAmamentacao()}',
+            anestesia = '{$paciente->getAnamnese()->getAnestesia()}',
+            doenca_grave = '{$paciente->getAnamnese()->getDoencaGrave()}',
+            vacinada = '{$paciente->getAnamnese()->getVacinada()}',
+            sentou = '{$paciente->getAnamnese()->getSentou()}',
+            engatinhou = '{$paciente->getAnamnese()->getEngatinhou()}',
+            andou = '{$paciente->getAnamnese()->getAndou()}',
+            falou = '{$paciente->getAnamnese()->getFalou()}',
+            dificuldade = '{$paciente->getAnamnese()->getDificuldade()}',
+            alegre = '{$paciente->getAnamnese()->getAlegre()}',
+            triste = '{$paciente->getAnamnese()->getTriste()}',
+            timido = '{$paciente->getAnamnese()->getTimido()}',
+            tranquilo = '{$paciente->getAnamnese()->getTranquilo()}',
+            inquieto = '{$paciente->getAnamnese()->getInquieto()}',
+            assustado = '{$paciente->getAnamnese()->getAssustado()}',
+            sono = '{$paciente->getAnamnese()->getSono()}',
+            posturanor = '{$paciente->getAnamnese()->getPosturaNormal()}',
+            posturaalt = '{$paciente->getAnamnese()->getPosturaAlterada()}',
+            fonacaonor = '{$paciente->getAnamnese()->getFonacaoNormal()}',
+            disturb_fala = '{$paciente->getAnamnese()->getDisturbiosFala()}',
+            paralisia = '{$paciente->getAnamnese()->getParalisia()}',
+            enurese = '{$paciente->getAnamnese()->getEnurese()}',
+            ctrl_esfinct = '{$paciente->getAnamnese()->getEsfincteres()}',
+            alimentacao = '{$paciente->getAnamnese()->getAlimentacao()}',
+            social = '{$paciente->getAnamnese()->getSociabilidade()}',
+            tiques = '{$paciente->getAnamnese()->getTiques()}',
+            fobias = '{$paciente->getAnamnese()->getFobias()}',
+            ansiedade = '{$paciente->getAnamnese()->getAnsiedade()}',
+            medo = '{$paciente->getAnamnese()->getMedo()}',
+            birra = '{$paciente->getAnamnese()->getBirra()}',
+            ciumes = '{$paciente->getAnamnese()->getCiumes()}',
+            observacoes = '{$paciente->getAnamnese()->getObservacoes()}'
+        
+            WHERE cpf_paciente = '{$paciente->getCpf()}';
+
+            ";
+        }
+    }else{
+        if ($paciente->getAnamnese()->getTipoParto()!='') {
+            $comandosSQL[] = "INSERT INTO anamnese_infantil
+            VALUES(
+                '{$paciente->getNome()}',
+                '{$paciente->getCpf()}',
+                '{$paciente->getAnamnese()->getHistoriaGestacao()}',
+                '{$paciente->getAnamnese()->getTipoParto()}',
+                '{$paciente->getAnamnese()->getProblemaParto()}',
+                '{$paciente->getAnamnese()->getAmamentacao()}',
+                '{$paciente->getAnamnese()->getAnestesia()}',
+                '{$paciente->getAnamnese()->getDoencaGrave()}',
+                '{$paciente->getAnamnese()->getVacinada()}',
+                '{$paciente->getAnamnese()->getSentou()}',
+                '{$paciente->getAnamnese()->getEngatinhou()}',
+                '{$paciente->getAnamnese()->getAndou()}',
+                '{$paciente->getAnamnese()->getFalou()}',
+                '{$paciente->getAnamnese()->getDificuldade()}',
+                '{$paciente->getAnamnese()->getAlegre()}',
+                '{$paciente->getAnamnese()->getTriste()}',
+                '{$paciente->getAnamnese()->getTimido()}',
+                '{$paciente->getAnamnese()->getTranquilo()}',
+                '{$paciente->getAnamnese()->getInquieto()}',
+                '{$paciente->getAnamnese()->getAssustado()}',
+                '{$paciente->getAnamnese()->getSono()}',
+                '{$paciente->getAnamnese()->getPosturaNormal()}',
+                '{$paciente->getAnamnese()->getPosturaAlterada()}',
+                '{$paciente->getAnamnese()->getFonacaoNormal()}',
+                '{$paciente->getAnamnese()->getDisturbiosFala()}',
+                '{$paciente->getAnamnese()->getParalisia()}',
+                '{$paciente->getAnamnese()->getEnurese()}',
+                '{$paciente->getAnamnese()->getEsfincteres()}',
+                '{$paciente->getAnamnese()->getAlimentacao()}',
+                '{$paciente->getAnamnese()->getSociabilidade()}',
+                '{$paciente->getAnamnese()->getTiques()}',
+                '{$paciente->getAnamnese()->getFobias()}',
+                '{$paciente->getAnamnese()->getAnsiedade()}',
+                '{$paciente->getAnamnese()->getMedo()}',
+                '{$paciente->getAnamnese()->getBirra()}',
+                '{$paciente->getAnamnese()->getCiumes()}',
+                '{$paciente->getAnamnese()->getObservacoes()}'
+                );
+            ";
+        }
+        $comandosSQL[] = "INSERT INTO anamnese_geral
+            VALUES (
+                '{$paciente->getCpf()}',
+                '{$paciente->getNome()}',
+                '{$paciente->getAnamnese()->getQueixaPrincipal()}',
+                '{$paciente->getAnamnese()->getHistoricoDoenca()}',
+                '{$paciente->getAnamnese()->getHemorragia()}',
+                '{$paciente->getAnamnese()->getReumatismo()}',
+                '{$paciente->getAnamnese()->getAlergia()}',
+                '{$paciente->getAnamnese()->getCardiovascular()}',
+                '{$paciente->getAnamnese()->getGastrite()}',
+                '{$paciente->getAnamnese()->getDiabetico()}',
+                '{$paciente->getAnamnese()->getDesmaio()}',
+                '{$paciente->getAnamnese()->getTratamento()}',
+                '{$paciente->getAnamnese()->getMedicamento()}',
+                '{$paciente->getAnamnese()->getOperacao()}',
+                '{$paciente->getAnamnese()->getManias()}',
+                '{$paciente->getAnamnese()->getDepressao()}',
+                '{$paciente->getAnamnese()->getTuberculose()}',
+                '{$paciente->getAnamnese()->getSarampo()}',
+                '{$paciente->getAnamnese()->getSifilis()}',
+                '{$paciente->getAnamnese()->getCaxumba()}',
+                '{$paciente->getAnamnese()->getHepatite()}',
+                '{$paciente->getAnamnese()->getVaricela()}',
+                '{$paciente->getAnamnese()->getAids()}',
+                '{$paciente->getAnamnese()->getOutra()}',
+                '{$paciente->getAnamnese()->getFumante()}',
+                '{$paciente->getAnamnese()->getFrequencia()}',
+                '{$paciente->getAnamnese()->getLabios()}',
+                '{$paciente->getAnamnese()->getMucosaJugal()}',
+                '{$paciente->getAnamnese()->getLingua()}',
+                '{$paciente->getAnamnese()->getSoalho()}',
+                '{$paciente->getAnamnese()->getPalatoDuro()}',
+                '{$paciente->getAnamnese()->getGarganta()}',
+                '{$paciente->getAnamnese()->getPalatoMole()}',
+                '{$paciente->getAnamnese()->getMucosaAlveolar()}',
+                '{$paciente->getAnamnese()->getGengivas()}',
+                '{$paciente->getAnamnese()->getGlandulasSalivares()}',
+                '{$paciente->getAnamnese()->getLinfonodos()}',
+                '{$paciente->getAnamnese()->getAtm()}',
+                '{$paciente->getAnamnese()->getMuscMastigadores()}',
+                '{$paciente->getAnamnese()->getOclusao()}',
+                '{$paciente->getAnamnese()->getAlteracoesEncontradas()}',
+                '{$paciente->getAnamnese()->getDiagnosticoPresuntivo()}',
+                '{$paciente->getAnamnese()->getExamesComplementares()}',
+                '{$paciente->getAnamnese()->getDiagnosticoDefinitivo()}',
+                '{$paciente->getAnamnese()->getPlanoTratamento()}'
+
             );
+        
+        
         ";
     }
-    $comandosSQL[] = "INSERT INTO anamnese_geral
-        VALUES (
-            '{$paciente->getCpf()}',
-            '{$paciente->getNome()}',
-            '{$paciente->getAnamnese()->getQueixaPrincipal()}',
-            '{$paciente->getAnamnese()->getHistoricoDoenca()}',
-            '{$paciente->getAnamnese()->getHemorragia()}',
-            '{$paciente->getAnamnese()->getReumatismo()}',
-            '{$paciente->getAnamnese()->getAlergia()}',
-            '{$paciente->getAnamnese()->getCardiovascular()}',
-            '{$paciente->getAnamnese()->getGastrite()}',
-            '{$paciente->getAnamnese()->getDiabetico()}',
-            '{$paciente->getAnamnese()->getDesmaio()}',
-            '{$paciente->getAnamnese()->getTratamento()}',
-            '{$paciente->getAnamnese()->getMedicamento()}',
-            '{$paciente->getAnamnese()->getOperacao()}',
-            '{$paciente->getAnamnese()->getManias()}',
-            '{$paciente->getAnamnese()->getDepressao()}',
-            '{$paciente->getAnamnese()->getTuberculose()}',
-            '{$paciente->getAnamnese()->getSarampo()}',
-            '{$paciente->getAnamnese()->getSifilis()}',
-            '{$paciente->getAnamnese()->getCaxumba()}',
-            '{$paciente->getAnamnese()->getHepatite()}',
-            '{$paciente->getAnamnese()->getVaricela()}',
-            '{$paciente->getAnamnese()->getAids()}',
-            '{$paciente->getAnamnese()->getOutra()}',
-            '{$paciente->getAnamnese()->getFumante()}',
-            '{$paciente->getAnamnese()->getFrequencia()}',
-            '{$paciente->getAnamnese()->getLabios()}',
-            '{$paciente->getAnamnese()->getMucosaJugal()}',
-            '{$paciente->getAnamnese()->getLingua()}',
-            '{$paciente->getAnamnese()->getSoalho()}',
-            '{$paciente->getAnamnese()->getPalatoDuro()}',
-            '{$paciente->getAnamnese()->getGarganta()}',
-            '{$paciente->getAnamnese()->getPalatoMole()}',
-            '{$paciente->getAnamnese()->getMucosaAlveolar()}',
-            '{$paciente->getAnamnese()->getGengivas()}',
-            '{$paciente->getAnamnese()->getGlandulasSalivares()}',
-            '{$paciente->getAnamnese()->getLinfonodos()}',
-            '{$paciente->getAnamnese()->getAtm()}',
-            '{$paciente->getAnamnese()->getMuscMastigadores()}',
-            '{$paciente->getAnamnese()->getOclusao()}',
-            '{$paciente->getAnamnese()->getAlteracoesEncontradas()}',
-            '{$paciente->getAnamnese()->getDiagnosticoPresuntivo()}',
-            '{$paciente->getAnamnese()->getExamesComplementares()}',
-            '{$paciente->getAnamnese()->getDiagnosticoDefinitivo()}',
-            '{$paciente->getAnamnese()->getPlanoTratamento()}'
-
-        );
-    
-    
-    ";
     for ($i = 0; $i < count($comandosSQL); $i++) {
         $conexao->query($comandosSQL[$i]);
         echo $conexao->error;
@@ -160,6 +252,19 @@ function verificaPacienteExistente(string $cpf)
     
     if($linha != NULL){
         return $linha['nome_paciente']; //já existe
+    }
+    return false; //não existe
+}
+
+function verificaAnamneseExistente(string $cpf, string $tabela)
+{
+    $conexao = novaConexao();
+    $comandoSQL = "SELECT * FROM  $tabela where cpf_paciente='{$cpf}';";
+    $resultado = mysqli_query($conexao, $comandoSQL);
+    $linha = mysqli_fetch_assoc($resultado);
+
+    if ($linha != NULL) {
+        return true; //já existe
     }
     return false; //não existe
 }
