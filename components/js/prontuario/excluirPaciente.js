b8f9f42ac9a8f7415b6bc490bfa355762f782f8c
Dibimo <diogobissolimoreno@gmail.com>
2021-05-11 19:15:44 -0300

    Evento do botão Excluir Paciente adicionado

---
+++ components/js/prontuario/excluirPaciente.js
@@ -0,0 +1,17 @@
+
+var botaoExcluir = document.querySelector("#excluirPaciente");
+botaoExcluir.addEventListener('click',function () {
+   var dados = new FormData();
+   dados.append("cpf_paciente", localStorage.getItem("cpf_paciente"));
+   var xml = new XMLHttpRequest();
+   xml.open("POST", "../../php/Funcoes/excluir_paciente.php", true);
+
+   xml.send(dados);
+   xml.onreadystatechange = function () {
+     if (xml.readyState == 4 && xml.status == 200) {
+        window.location.href = "/index.html";
+     }
+   };
+
+});
+
