b23cc06fe0ee164549d4a9e1d8f25a7c3cbefb7e
Dibimo <diogobissolimoreno@gmail.com>
2021-05-17 14:19:38 -0300

    Melhorias na edição dos campos

--- components/js/prontuario/editarCampos.js
+++ components/js/prontuario/editarCampos.js
@@ -1,37 +1,146 @@
 var botaoEditar = document.querySelector('#editarEndereco');
 var botaoSalvar = document.querySelector('#salvarNovosDados');
+let excecoes = {
+  cor: ["branco", "pardo", "negro"],
+  escolaridade: [
+    "infantil",
+    "fundamental",
+    "medio",
+    "superior",
+    "posGraduacao",
+    "mestrado",
+    "doutorado",
+  ],
+  estado_civil: ["solteiro", "casado", "separado", "divorciado", "viuvo"],
+  estado_mora: [
+    "ac",
+    "al",
+    "ap",
+    "am",
+    "ba",
+    "ce",
+    "df",
+    "es",
+    "go",
+    "ma",
+    "mt",
+    "ms",
+    "mg",
+    "pa",
+    "pb",
+    "pr",
+    "pe",
+    "pi",
+    "rj",
+    "rn",
+    "rs",
+    "ro",
+    "rr",
+    "sc",
+    "sp",
+    "se",
+    "to",
+  ],
+  estado_nasc: [
+    "ac",
+    "al",
+    "ap",
+    "am",
+    "ba",
+    "ce",
+    "df",
+    "es",
+    "go",
+    "ma",
+    "mt",
+    "ms",
+    "mg",
+    "pa",
+    "pb",
+    "pr",
+    "pe",
+    "pi",
+    "rj",
+    "rn",
+    "rs",
+    "ro",
+    "rr",
+    "sc",
+    "sp",
+    "se",
+    "to",
+  ],
+};
+
+
 
 botaoEditar.addEventListener('click',function () {
    botaoSalvar.classList.remove('invisivel');
-   var campos = document.querySelectorAll('div[id="dadosEndereco"] div div');
+   var campos = document.querySelectorAll('.campoEditavel');
+
    campos.forEach(campo => {
        let regex = /(:\s)(.*)/mg;
-       let dadoAntigo = campo.lastChild.textContent;
+       let dadoAntigo = campo.textContent;
        dadoAntigo = (`${regex.exec(dadoAntigo)[2]}`);
-       campo.lastChild.textContent = campo.lastChild.textContent.replace(dadoAntigo,"");
-       var novoInput = document.createElement('input');
+       campo.textContent = campo.textContent.replace(dadoAntigo,"");
+       let nomesExcecoes = Object.keys(excecoes);
+       if(nomesExcecoes.includes(campo.getAttribute('name'))){
+           var novoInput = constroiSelect(campo.getAttribute('name'),dadoAntigo);
+       }else{
+           var novoInput = document.createElement('input');
+       }
        campo.appendChild(novoInput);
        novoInput.value = dadoAntigo;
        novoInput.classList.add('novosDados');
-       
-       
    });
 });
 
 botaoSalvar.addEventListener('click',function () {
-    this.classList.add('invisivel');
     var inputs = document.querySelectorAll('.novosDados');
-    var labels = campos = document.querySelectorAll('div[id="dadosEndereco"] div div label');
-    const novosDados = new FormData();
-    novosDados.append('cpf_paciente',cpf);
-    for (let i = 0; i < inputs.length; i++) {
-        labels[i].textContent += inputs[i].value;
-        novosDados.append(labels[i].getAttribute('name'),inputs[i].value);
-        inputs[i].remove();
-    }
+    var labels  = document.querySelectorAll('.campoEditavel');
 
+    let novosDados = new FormData();
+    novosDados.append('cpf_paciente',localStorage["cpf_paciente"]);
+    for (let i = 0; i < inputs.length; i++) {
+        let valor = inputs[i].value;
+        inputs[i].remove();
+        labels[i].textContent += valor;
+        novosDados.append(labels[i].getAttribute('name'),valor);
+    }
     var xml = new XMLHttpRequest();
-    xml.open('POST','../../php/Funcoes/atualiza_dados_endereco.php',true);
+    xml.onreadystatechange = function () {
+        botaoSalvar.textContent = 'Salvando...';
+        if (xml.readyState == 4 && xml.status == 200) {
+            botaoSalvar.textContent = "Salvar";
+            botaoSalvar.classList.add('invisivel');
+      }
+    };
+    xml.open("POST", "../../php/Funcoes/atualiza_dados.php", true);
     xml.send(novosDados);
     
 });
+
+
+function constroiSelect(tipo, dadoAtual){
+    let select = document.createElement("select");
+    let dadoAtualOption = document.createElement("option");
+    
+    //adicionando a opção atual na primeira posição da lista
+    dadoAtualOption.textContent = dadoAtual;
+    dadoAtualOption.value = dadoAtual;
+
+    select.appendChild(dadoAtualOption);
+
+    const posicaoDadoAtual = excecoes[tipo].indexOf(dadoAtual);
+    if(posicaoDadoAtual > -1){
+        excecoes[tipo].splice(posicaoDadoAtual, 1); //removendo a opção atual da lista
+        excecoes[tipo].forEach((opcao)=>{
+            let novaOpcao = document.createElement("option");
+            novaOpcao.textContent = opcao;
+            novaOpcao.value= opcao;
+            select.appendChild(novaOpcao);
+        });
+    }
+    
+    return select;
+}
