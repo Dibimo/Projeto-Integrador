388661efed964bfa3b0892f61b55c15259e44db5
Dibimo <diogobissolimoreno@gmail.com>
2021-05-17 14:18:09 -0300

    Adicionada formatação para a data de procedimento

--- components/php/Funcoes/atualiza_prontuario.php
+++ components/php/Funcoes/atualiza_prontuario.php
@@ -5,7 +5,12 @@ $cpf = filter_input(INPUT_POST, 'cpf_paciente', FILTER_SANITIZE_STRING);
 $dataProcedimento = filter_input(INPUT_POST, 'dataProcedimento', FILTER_SANITIZE_STRING);
 $procedimentoTexto = filter_input(INPUT_POST, 'procedimentoTexto', FILTER_SANITIZE_STRING);
 
+//TRATAMENTO DA DATA
 
+$ano = explode('/',$dataProcedimento)[2];
+$mes = explode('/',$dataProcedimento)[1];
+$dia = explode('/',$dataProcedimento)[0];
+$dataProcedimento = $ano.'-'.$mes.'-'.$dia;
 
 atualizaProntuario($cpf,$dataProcedimento,$procedimentoTexto);
 
