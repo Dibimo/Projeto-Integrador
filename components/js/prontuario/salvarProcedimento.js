af8b6f17ef347aa54aab3d699984564670143c3b
Dibimo <diogobissolimoreno@gmail.com>
2021-05-17 14:45:49 -0300

    Mensagem de confirmação de salvamento de dados implementada

--- components/js/prontuario/salvarProcedimento.js
+++ components/js/prontuario/salvarProcedimento.js
@@ -2,9 +2,26 @@ function salvarProcedimento(dataProcedimento,procedimentoTexto,cpfPaciente) {
     var xml = new XMLHttpRequest();
     var dados = new FormData();
 
+    let mensagemDeSalvamento = document.querySelector("#mensagemDeSalvamento");
+    mensagemDeSalvamento.textContent = "Salvando...";
+
     dados.append('cpf_paciente',cpfPaciente);
     dados.append('dataProcedimento',dataProcedimento);
     dados.append('procedimentoTexto',procedimentoTexto);
+
+    xml.onreadystatechange = function () {
+      if (xml.readyState == 4 && xml.status == 200) {
+        let timer = 1;
+        mensagemDeSalvamento.textContent = "Os dados foram salvos!";
+        let contador = setInterval(() => {
+          timer += 1;
+          if(timer === 5){
+              mensagemDeSalvamento.textContent = " ";
+              clearInterval(contador);
+          }
+        }, 1000);
+      }
+    };
 
     xml.open('POST','../../php/Funcoes/atualiza_prontuario.php',true);
     xml.send(dados);
